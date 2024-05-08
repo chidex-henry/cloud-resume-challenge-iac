6fba15b5910227a83d81e754a6867aaae56bb8ec
Chidi Ukaegbu <chidex.ukaegbu4real@gmail.com>
2024-05-08 13:10:56 -0500

    provoder and backend config

---
+++ variables.tf
@@ -0,0 +1,16 @@
+#environment variables 
+variable "project_name" {
+  description = "project name"
+  type        = string
+}
+
+variable "environment" {
+  description = "environment"
+  type        = string
+}
+
+#s3 variables 
+variable "web_file_bucket_name" {
+  description = "s3 bucket name"
+  type        = string
+}
